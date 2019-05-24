928bee64f319de7de895e7ddc083721b5f213f1a
sachin <sachin17361@iiitd.ac.in>
2019-05-24 17:32:25 +0800

    dialog

--- angular/conFusion/src/app/app.module.ts
+++ angular/conFusion/src/app/app.module.ts
@@ -1,14 +1,17 @@
+import 'hammerjs';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import {MatListModule  } from '@angular/material/list';
-import { AppComponent } from './app.component';
 import { MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
-import 'hammerjs';
+import { AppRoutingModule} from './app-routing/app-routing.module';
+import { MatDialogModule } from '@angular/material/dialog';
+
+import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailsComponent } from './dishdetails/dishdetails.component';
 import { HeaderComponent } from './header/header.component';
@@ -16,10 +19,12 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
-import { AppRoutingModule} from './app-routing/app-routing.module';
 import { LeaderService } from './services/leader.service';
+
 
 @NgModule({
   declarations: [
@@ -30,7 +35,8 @@ import { LeaderService } from './services/leader.service';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
   BrowserAnimationsModule,
@@ -41,8 +47,10 @@ import { LeaderService } from './services/leader.service';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule
   ],
+  entryComponents:[LoginComponent],
   providers: [DishService,PromotionService,LeaderService],
   bootstrap: [AppComponent]
 })
